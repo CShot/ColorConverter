ee429afd382acd4d0eaf1ddf242f7664ee9d3836
Максим Панков < >
2017-03-02 23:33:21 +0300

    ColorConverter 1.0

---
+++ Pankow_ColorConverter/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Pankow_ColorConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.размерОбработкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.всёИзображениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.леваяПоловинаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.праваяПоловинаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.возвратToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(722, 315);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.настройкиToolStripMenuItem,
+            this.возвратToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(722, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.размерОбработкиToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // размерОбработкиToolStripMenuItem
+            // 
+            this.размерОбработкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.всёИзображениеToolStripMenuItem,
+            this.леваяПоловинаToolStripMenuItem,
+            this.праваяПоловинаToolStripMenuItem});
+            this.размерОбработкиToolStripMenuItem.Name = "размерОбработкиToolStripMenuItem";
+            this.размерОбработкиToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.размерОбработкиToolStripMenuItem.Text = "Размер обработки";
+            // 
+            // всёИзображениеToolStripMenuItem
+            // 
+            this.всёИзображениеToolStripMenuItem.Name = "всёИзображениеToolStripMenuItem";
+            this.всёИзображениеToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.всёИзображениеToolStripMenuItem.Text = "Всё изображение";
+            this.всёИзображениеToolStripMenuItem.Click += new System.EventHandler(this.всёИзображениеToolStripMenuItem_Click);
+            // 
+            // леваяПоловинаToolStripMenuItem
+            // 
+            this.леваяПоловинаToolStripMenuItem.Name = "леваяПоловинаToolStripMenuItem";
+            this.леваяПоловинаToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.леваяПоловинаToolStripMenuItem.Text = "Левая половина";
+            this.леваяПоловинаToolStripMenuItem.Click += new System.EventHandler(this.леваяПоловинаToolStripMenuItem_Click);
+            // 
+            // праваяПоловинаToolStripMenuItem
+            // 
+            this.праваяПоловинаToolStripMenuItem.Name = "праваяПоловинаToolStripMenuItem";
+            this.праваяПоловинаToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.праваяПоловинаToolStripMenuItem.Text = "Правая половина";
+            this.праваяПоловинаToolStripMenuItem.Click += new System.EventHandler(this.праваяПоловинаToolStripMenuItem_Click);
+            // 
+            // возвратToolStripMenuItem
+            // 
+            this.возвратToolStripMenuItem.Name = "возвратToolStripMenuItem";
+            this.возвратToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.возвратToolStripMenuItem.Text = "Возврат";
+            this.возвратToolStripMenuItem.Visible = false;
+            this.возвратToolStripMenuItem.Click += new System.EventHandler(this.возвратToolStripMenuItem_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar1.LargeChange = 30;
+            this.trackBar1.Location = new System.Drawing.Point(0, 343);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Minimum = -180;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(577, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickFrequency = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(583, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar2.LargeChange = 30;
+            this.trackBar2.Location = new System.Drawing.Point(0, 394);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Minimum = -50;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(577, 45);
+            this.trackBar2.SmallChange = 10;
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.TickFrequency = 10;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar3.LargeChange = 30;
+            this.trackBar3.Location = new System.Drawing.Point(0, 445);
+            this.trackBar3.Maximum = 50;
+            this.trackBar3.Minimum = -50;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(577, 45);
+            this.trackBar3.SmallChange = 10;
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.TickFrequency = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(583, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 33);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Яркость";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(583, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Контрастность";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 498);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem размерОбработкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem всёИзображениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem леваяПоловинаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem праваяПоловинаToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem возвратToolStripMenuItem;
+    }
+}
+
---
+++ Pankow_ColorConverter/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pankow_ColorConverter
+{
+    public partial class Form1 : Form
+    {
+
+
+        Stack undoStack_valColor = new Stack();//Стек,отвечающий за значение измененного цвета
+        Stack undoStack_valOfParameterATypeChange = new Stack();//Стек,отвечающий за вид измения цвета
+        Stack undoStack_valOfParameter_1 = new Stack();//Стек,отвечающий за деление изображения
+
+        ColorConvert cc = new ColorConvert();
+
+        int valueOfParameter_1 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void ColorChange(int val)//Функция изменения цвета
+        {
+            int width = cc.rgbImage.Width;
+            int height = cc.rgbImage.Height;
+            int tmpA = 0;
+            int tmpB = 0;
+            int tmpWidth = width;
+            int tmpHeight = height;
+            int[] halfArr = { 0, 0, 0, 0 };
+            halfArr = HalfChange(valueOfParameter_1, width, height, tmpA, tmpB, tmpWidth, tmpHeight);
+            tmpA = halfArr[0];
+            tmpB = halfArr[1];
+            tmpWidth = halfArr[2];
+            tmpHeight = halfArr[3];
+            cc.CovertRGBimagetoHSV();
+            for (int i = tmpA; i < tmpWidth; ++i)
+            {
+                for (int j = tmpB; j < tmpHeight; ++j)
+                {
+                    int tmpH = cc.hsvImage[i, j].h + val;
+                    if (tmpH < 0)
+                    {
+                        cc.hsvImage[i, j].h = (ushort)(tmpH + 360);
+                    }
+                    else if (tmpH >= 360)
+                    {
+                        cc.hsvImage[i, j].h = (ushort)(tmpH - 360);
+                    }
+                    else
+                    {
+                        cc.hsvImage[i, j].h = (ushort)tmpH;
+                    }
+                }
+            }
+            cc.CovertHSVimagetoRGB();
+            pictureBox1.Image = cc.rgbImage;
+        }
+
+        public void BrightnessChange(int val)//Функция изменения яркости
+        {
+            int width = cc.rgbImage.Width;
+            int height = cc.rgbImage.Height;
+            int tmpA = 0;
+            int tmpB = 0;
+            int tmpWidth = width;
+            int tmpHeight = height;
+            cc.CovertRGBimagetoHSV();
+            int[] halfArr = { 0, 0, 0, 0 };
+            halfArr = HalfChange(valueOfParameter_1, width, height, tmpA, tmpB, tmpWidth, tmpHeight);
+            tmpA = halfArr[0];
+            tmpB = halfArr[1];
+            tmpWidth = halfArr[2];
+            tmpHeight = halfArr[3];
+            for (int i = tmpA; i < tmpWidth; ++i)
+            {
+                for (int j = tmpB; j < tmpHeight; ++j)
+                {
+                    int tmpV = cc.hsvImage[i, j].v + val;
+                    if (tmpV < 0)
+                    {
+                        cc.hsvImage[i, j].v = 0;
+                    }
+                    else if (tmpV >= 100)
+                    {
+                        cc.hsvImage[i, j].v = 100;
+                    }
+                    else
+                    {
+                        cc.hsvImage[i, j].v = (byte)tmpV;
+                    }
+                }
+            }
+            cc.CovertHSVimagetoRGB();
+            pictureBox1.Image = cc.rgbImage;
+        }
+
+        public void ContrastValChange(int val)//Функция изменения контраста
+        {
+            int width = cc.rgbImage.Width;
+            int height = cc.rgbImage.Height;
+            int tmpA = 0;
+            int tmpB = 0;
+            int tmpWidth = width;
+            int tmpHeight = height;
+            int[] halfArr = { 0, 0, 0, 0 };
+            halfArr = HalfChange(valueOfParameter_1, width, height, tmpA, tmpB, tmpWidth, tmpHeight);
+            tmpA = halfArr[0];
+            tmpB = halfArr[1];
+            tmpWidth = halfArr[2];
+            tmpHeight = halfArr[3];
+            cc.CovertRGBimagetoHSV();
+            for (int i = tmpA; i < tmpWidth; ++i)
+            {
+                for (int j = tmpB; j < tmpHeight; ++j)
+                {
+                    int tmpS = cc.hsvImage[i, j].v + val;
+                    if (tmpS < 0)
+                    {
+                        cc.hsvImage[i, j].s = 0;
+                    }
+                    else if (tmpS >= 100)
+                    {
+                        cc.hsvImage[i, j].s = 100;
+                    }
+                    else
+                    {
+                        cc.hsvImage[i, j].s = (byte)tmpS;
+                    }
+                }
+            }
+
+            cc.CovertHSVimagetoRGB();
+            pictureBox1.Image = cc.rgbImage;
+            
+            
+        }
+
+        private int[] HalfChange(int valHalf, int width, int height, int tmpA, int tmpB, int tmpWidth, int tmpHeight)
+        {
+            int[] arr = { 0, 0, 0, 0 };
+
+            if (valHalf == 0)//деление изображния на 2 части
+            {
+                arr[0] = 0; arr[1] = 0; arr[2] = width; arr[3] = height;
+                undoStack_valOfParameter_1.Push(0);
+            }
+
+            else if (valHalf == 1)
+            {
+                arr[0] = 0; arr[1] = 0; arr[2] = width / 2; arr[3] = height;
+                undoStack_valOfParameter_1.Push(1);
+            }
+
+            else if (valHalf == 2)
+            {
+                arr[0] = width / 2; arr[1] = 0; arr[2] = width; arr[3] = height;
+                undoStack_valOfParameter_1.Push(2);
+            }
+            return arr;
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string imagePath;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "image files|*.jpg;*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = dialog.FileName;
+                cc.rgbImage = new Bitmap(imagePath);
+                this.pictureBox1.Image = cc.rgbImage;
+            }
+        }
+
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "image files|*.jpg;*.png";
+            saveDialog.FileName = "Image";
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)//Цвет
+        {
+
+            this.Cursor = Cursors.WaitCursor;
+            ColorChange(trackBar1.Value);
+            undoStack_valColor.Push(trackBar1.Value);
+            undoStack_valOfParameterATypeChange.Push(0);
+            this.Cursor = Cursors.Default;
+            trackBar1.Value = 0;
+            if (undoStack_valColor.Peek() != null)
+            {
+                this.возвратToolStripMenuItem.Visible = true;
+            }
+            else
+            {
+                this.возвратToolStripMenuItem.Visible = false;
+            }
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)//Яркость
+        {
+            this.Cursor = Cursors.WaitCursor;
+            BrightnessChange(trackBar2.Value);
+            undoStack_valColor.Push(trackBar2.Value);
+            undoStack_valOfParameterATypeChange.Push(1);
+            this.Cursor = Cursors.Default;
+            trackBar2.Value = 0;
+            if (undoStack_valColor.Peek() != null)
+            {
+                this.возвратToolStripMenuItem.Visible = true;
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)//Контрастность
+        {
+            this.Cursor = Cursors.WaitCursor;
+            ContrastValChange(trackBar3.Value);
+            undoStack_valColor.Push(trackBar3.Value);
+            undoStack_valOfParameterATypeChange.Push(2);
+            this.Cursor = Cursors.Default;
+            trackBar3.Value = 0;
+            if (undoStack_valColor.Peek() != null)
+            {
+                this.возвратToolStripMenuItem.Visible = true;
+            }
+        }
+
+
+        private void всёИзображениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            valueOfParameter_1 = 0;
+        }
+
+
+        private void леваяПоловинаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            valueOfParameter_1 = 1;
+        }
+
+
+        private void праваяПоловинаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            valueOfParameter_1 = 2;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            /*if (tmpStack == true)
+            {
+                object stackPopElemColor = undoStack_valColor.Pop();
+                int stackElemColor = Convert.ToInt32(stackPopElemColor);
+
+                object stackPopElemvalOfParameter_1 = undoStack_valOfParameter_1.Pop();
+                int stackElemvalueOfParameter_1 = Convert.ToInt32(stackPopElemvalOfParameter_1);
+
+                object stackPopElemvalOfParameter_2 = undoStack_valOfParameter_2.Pop();
+                int stackElemvalOfParameter_2 = Convert.ToInt32(stackPopElemvalOfParameter_2);
+                trackBar2.Value = (-1) * stackElemColor;
+            }*/
+        }
+
+        private void возвратToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            trackBar1.Value = 0;
+            trackBar2.Value = 0; 
+            trackBar3.Value = 0;
+            try
+            {
+                if (undoStack_valColor.Peek() != null)
+                {
+                    this.возвратToolStripMenuItem.Visible = true;
+                }
+
+                object stackPopElemColor = undoStack_valColor.Pop();
+                int stackElemColor = Convert.ToInt32(stackPopElemColor);
+
+                object stackPopElemvalOfParameter_1 = undoStack_valOfParameter_1.Pop();
+                int stackElemvalueOfParameter_1 = Convert.ToInt32(stackPopElemvalOfParameter_1);
+
+                object stackPopElemOfParameterATypeChange = undoStack_valOfParameterATypeChange.Pop();
+                int stackElemvalOfParameterATypeChange = Convert.ToInt32(stackPopElemOfParameterATypeChange);
+
+                int tmpTypeChange = valueOfParameter_1;
+
+                if (stackElemvalueOfParameter_1 == 0)
+                {
+                    valueOfParameter_1 = 0;
+                    if (stackElemvalOfParameterATypeChange == 0)
+                    {
+                        ColorChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 1)
+                    {
+                        BrightnessChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 2)
+                    {
+                        ContrastValChange((-1) * stackElemColor);
+                    }
+                }
+
+                if (stackElemvalueOfParameter_1 == 1)
+                {
+                    valueOfParameter_1 = 1;
+                    if (stackElemvalOfParameterATypeChange == 0)
+                    {
+                        ColorChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 1)
+                    {
+                        BrightnessChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 2)
+                    {
+                       ContrastValChange((-1) * stackElemColor);
+                    }
+                }
+
+                if (stackElemvalueOfParameter_1 == 2)
+                {
+                    valueOfParameter_1 = 2;
+                    if (stackElemvalOfParameterATypeChange == 0)
+                    {
+                        ColorChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 1)
+                    {
+                        BrightnessChange((-1) * stackElemColor);
+                    }
+                    if (stackElemvalOfParameterATypeChange == 2)
+                    {
+                        ContrastValChange((-1) * stackElemColor);
+                    }
+                }
+
+               /* object tmpEmpty = undoStack_valColor.Peek();
+                int intTmpEmty = Convert.ToInt32(tmpEmpty);
+
+                if (intTmpEmty == 0)
+                {
+                    this.возвратToolStripMenuItem.Visible = false;
+                }*/
+
+            }
+            catch
+            {
+                this.возвратToolStripMenuItem.Visible = false;
+            }
+        }
+
+
+
+    }
+}
